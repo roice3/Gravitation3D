38b1674eb0ab79929dbd20f262ee32ef3950580e
roice3 <roice3@gmail.com>
2015-12-14 00:34:45 -0600

    Upgrade solution to VS2015 and get it building.
    The setup projects (both for Gravitation3D and the screen saver) could
    not be upgraded, so I've just removed those for now.  I'm thinking I may
    not want to have installers anymore anyway.

--- Gravitation3D/stdafx.h
+++ Gravitation3D/stdafx.h
@@ -10,7 +10,7 @@
 
 // Modify the following defines if you have to target a platform prior to the ones specified below.
 // Refer to MSDN for the latest info on corresponding values for different platforms.
-#ifndef WINVER				// Allow use of features specific to Windows 95 and Windows NT 4 or later.
+/*#ifndef WINVER				// Allow use of features specific to Windows 95 and Windows NT 4 or later.
 #define WINVER 0x0400		// Change this to the appropriate value to target Windows 98 and Windows 2000 or later.
 #endif
 
@@ -24,7 +24,7 @@
 
 #ifndef _WIN32_IE			// Allow use of features specific to IE 4.0 or later.
 #define _WIN32_IE 0x0400	// Change this to the appropriate value to target IE 5.0 or later.
-#endif
+#endif*/
 
 #define _ATL_CSTRING_EXPLICIT_CONSTRUCTORS	// some CString constructors will be explicit
 
